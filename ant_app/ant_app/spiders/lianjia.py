516e820edf7dd78812d8518698090fd1f1becb17
yiosushh <xuchao0802@aliyun.com>
2019-04-05 22:42:37 +0800

    添加注释

--- ant_app/ant_app/spiders/lianjia.py
+++ ant_app/ant_app/spiders/lianjia.py
@@ -64,7 +64,7 @@ class LianjiaSpider(scrapy.Spider):
     def parse(self, response):
         pass
 
-    def get_headers(self,type):
+    def get_headers(self,type):#返回header
         if type == 1:
             headers = {
                 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
