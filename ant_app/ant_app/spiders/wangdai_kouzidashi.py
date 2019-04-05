8e6c2386598cca464037dc85d0399f8a1dc0e47d
yiosus <xuchao0802@aliyun.com>
2019-04-05 21:54:38 +0800

    scrapy初始版

--- ant_app/ant_app/spiders/wangdai_kouzidashi.py
+++ ant_app/ant_app/spiders/wangdai_kouzidashi.py
@@ -85,5 +85,3 @@ class KouzidashiSpider(RedisSpider):
                 "user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
             }
         return headers
-
-
